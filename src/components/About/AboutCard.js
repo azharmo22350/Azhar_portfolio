86876fc688fd72d3eb782d00ac2e6ab04fc36e94
azharmo22350 <azharmohammed22350@gmail.com>
2024-01-01 21:10:09 +0530

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Mohamed Azhar </span>
+            from <span className="purple"> Kurunegala,Sri Lanka.</span>
+            <br />
+            I was  employed as a software developer at GTN Technologies(Pvt) Ltd.
+            <br />
+            I have completed Bsc(Hons) in Engineering at University Of Moratuwa ,Sri Lanka
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
